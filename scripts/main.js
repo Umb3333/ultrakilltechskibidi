87f4a99df77d7519631bad30ae4a1cfb8657d6c7
Umb3333 <148855458+Umb3333@users.noreply.github.com>
2025-03-04 13:10:36 +0100

    skbidsf

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,11 @@ const output = document.querySelector('.output');
 const button1 = document.querySelector('.bind1-btn');
 const button2 = document.querySelector('.bind2-btn');
 
+const keyBindings = {
+    button1: "CTRL", 
+    button2: "SPACE", 
+};
+
 function handleKeyDown(button) {
     return function () {
         button.textContent = "BIND.."; 
@@ -15,7 +20,8 @@ function handleKeyDown(button) {
 
             if (e.key === "Escape") {
                 
-                button.textContent = "BIND";
+                button.textContent = keyBindings[keyBindingKey];
+                
                 document.removeEventListener('keydown', keyDownHandler);
                 return;
             }
@@ -38,10 +44,39 @@ button1.addEventListener('click', handleKeyDown(button1));
 button2.addEventListener('click', handleKeyDown(button2));
 
 
+function handleStart() {
+    let FirstTStamp = null; 
+    let SecondTStamp = null; 
+
+    document.addEventListener('keydown', (e) => {
+        
+        if (e.key === button1.textContent.toLowerCase()) {
+            FirstTStamp = performance.now(); 
+            console.log(`Button 1 key pressed at: ${FirstTStamp}`);
+        }
+
+        if (e.key === button2.textContent.toLowerCase()) {
+            SecondTStamp = performance.now();
+            console.log(`Button 2 key pressed at: ${SecondTStamp}`);
+        }
+
+      
+        if (FirstTStamp && SecondTStamp) {
+            const timeDifference = SecondTStamp - FirstTStamp;
+            console.log(`Time difference between button1 and button2: ${timeDifference} ms`);
+        }
+    });
+}
+
 start.addEventListener('click', () => {
-var allowedkeys = {button1: button1.textContent, button2: button2.textContent};
-var pattern = {button1: button1.textContent, button2: button2.textContent};
-var key = allowedKeys[e.keyCode];   
-console.log(allowedkeys);
+// var allowedkeys = {button1: button1.textContent, button2: button2.textContent};
+// var pattern = {button1: button1.textContent, button2: button2.textContent};
+// var key = allowedKeys[e.keyCode];
+handleStart();
+
+
+
+
+
 
 });
